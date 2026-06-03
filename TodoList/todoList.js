86cfd3f53b9c06b2e4a6bb206b1cc97db9d95159
Aditya Pratap Singh <adityapratap141003@gmail.com>
2026-06-03 22:53:04 +0530

    Todo List: Added delete button. Yayyyy!

--- TodoList/todoList.js
+++ TodoList/todoList.js
@@ -6,20 +6,42 @@ const divElement = document.querySelector('.all-tasks');
 
 renderTasks();
 
-addTaskButton.addEventListener('click', () => addTask());
-inputElement.addEventListener('keydown', () => addTask(event));
+addTaskButton.addEventListener('click', addTask);
+inputElement.addEventListener('keydown', addTask);
+divElement.addEventListener('click', deleteTask);
 
 function renderTasks() {
     let todoListHTML = ``;
-    for(task of tasks) {
-        todoListHTML += `<p>${task}</p>`;
+    for(let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        todoListHTML += `
+        <p>
+            ${task}
+            <button data-index="${i}" data-kaam="delete">Delete</button>
+        </p>
+    `;
     } divElement.innerHTML = todoListHTML;
 }
 
-function addTask(event) {
-    if((event && event.key === 'Enter') || !event) {
+function addTask() {
+    if(!inputElement.value) {
+        return;
+    }
+
+    console.log(event);
+
+    if(event.key === 'Enter' || event.type === 'click') {
         tasks.push(inputElement.value);
         inputElement.value = '';
+        renderTasks();
+    }
+}
+
+function deleteTask() {
+    const targ = event.target;
+    if(targ.dataset.kaam === 'delete') {
+        const idx = Number(targ.dataset.index);
+        tasks.splice(idx, 1);
         renderTasks();
     }
 }
