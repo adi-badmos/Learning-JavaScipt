aa1f962e9470c114322240e567d769f5c49dfd46
Aditya Pratap Singh <adityapratap141003@gmail.com>
2026-06-07 19:39:39 +0530

    Amazon: Delete products from cart when delete button is clicked

--- Amazon/data/cart.js
+++ Amazon/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 }, {
@@ -32,4 +32,8 @@ export function updateCartQuantity() {
 
     const cartQuantityElement = document.querySelector('.js-cart-quantity');
     cartQuantityElement.innerHTML = cartQuantity;
+}
+
+export function removeFromCart(productId) {
+    cart = cart.filter(cartItem => cartItem.productId !== productId);
 }
--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -1,9 +1,10 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = ``;
 
+// Generating HTML
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
@@ -15,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -37,7 +38,7 @@ cart.forEach((cartItem) => {
                         <span class="update-quantity-link link-primary">
                             Update
                         </span>
-                        <span class="delete-quantity-link link-primary">
+                        <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                             Delete
                         </span>
                     </div>
@@ -92,5 +93,19 @@ cart.forEach((cartItem) => {
     `;
 });
 
+// Using the generated HTML
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
+
+// When Delete button is clicked, remove the product from the cart and update the HTML
+document.querySelectorAll('.js-delete-link')
+    .forEach((deleteLink) => {
+        deleteLink.addEventListener('click', () => {
+            const productId = deleteLink.dataset.productId;
+            removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+            container.remove();
+        });
+    });
