b718be16d32b71e5f2791dc037c57335b21c3256
Aditya Pratap Singh <adityapratap141003@gmail.com>
2026-06-10 22:43:56 +0530

    Amazon: Will be working on External libraries

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -1,6 +1,10 @@
 import { cart, removeFromCart, updateCartQuantity, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'day');
 
 let cartSummaryHTML = ``;
 
