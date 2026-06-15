8efe5688c07ca378f9c5896ad713b19c5c8e6531
adi-badmos <adityapratap141003@gmail.com>
2026-06-15 23:44:18 +0530

    Amazon: Some more updates, after payment summary

--- Amazon/scripts/checkout/orderSummary.js
+++ Amazon/scripts/checkout/orderSummary.js
@@ -1,6 +1,6 @@
 import { cart, removeFromCart, updateCartQuantity, updateQuantity, updateDeliveryOption } from '../../data/cart.js';
 import { products, getProduct } from '../../data/products.js';
-import formatCurrency from '../utils/money.js';
+import { formatCurrency } from '../utils/money.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
