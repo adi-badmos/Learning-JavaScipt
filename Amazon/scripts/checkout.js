4a6100f28c16a1459c7196fa17f4d512c6762f68
adi-badmos <adityapratap141003@gmail.com>
2026-06-18 11:01:55 +0530

    Amazon: Backend practice

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { cart } from "../data/cart-class.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
